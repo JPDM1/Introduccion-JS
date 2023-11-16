4254b38b4d87601c168d1312562d38db57fdddd4
JPDM1 <juanpablodelzo@gmail.com>
2023-11-16 16:50:01 +0100

    ejercicios 16_11

---
+++ ejercicios2.js
@@ -0,0 +1,8 @@
+// Calcular si un número es mayor a otro y también si son iguales
+var n1=5;
+var n2=10;
+console.log(n1);
+console.log(n2);
+if(n1>n2){console.log(`${n1} es mayor a ${n2}`);
+} else if(n1==n2){console.log(`ambos son iguales`);
+} else { console.log(`${n1} es menor que ${n2}`)};  
