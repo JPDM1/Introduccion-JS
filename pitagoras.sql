a2e32cf6cfe7f171fce7b11c431ea3e8d8b0c300
JPDM1 <juanpablodelzo@gmail.com>
2023-11-30 16:38:43 +0100

    Textos de informacion

--- pitagoras.sql
+++ pitagoras.sql
@@ -3,6 +3,7 @@ USE test;
 -- tabla
 DROP TABLE IF EXISTS triangulo;
 CREATE TABLE triangulo(a FLOAT NOT NULL , b FLOAT NOT NULL,c FLOAT );
+-- disparador
 DROP TRIGGER IF EXISTS tp;
 DELIMITER //
 CREATE TRIGGER tp BEFORE INSERT ON triangulo FOR EACH ROW
@@ -12,6 +13,8 @@ SET h= SQRT(POW(NEW.a,2)+POW(NEW.b,2));
 SET NEW.c=ROUND(h,2);
 END
 // DELIMITER ;
+-- Insertar
 INSERT INTO triangulo VALUES 
 (2.2,3,DEFAULT),(2.7,7.8,DEFAULT);
+-- Consultar
 SELECT * FROM triangulo;
